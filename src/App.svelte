3037393ac6fe9a0a503328f10de9648ab56afea0
Denis Vigouroux <dvigouro@gmail.com>
2022-10-23 18:55:04 +0200

    Create SmearViewer component

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,11 @@
 <script>
-  import SmearImage from "./lib/SmearImage.svelte";
+  import SmearViewer from "./lib/SmearViewer.svelte";
 </script>
 
 <main>
   <div>
     <h1>Plasmodium View</h1>
-    <SmearImage smearName={"falciparum"} imageName={"pf220517213207.jpg"} />
+    <SmearViewer smearName={"falciparum"} />
   </div>
 </main>
 
---
+++ src/lib/SmearViewer.svelte
@@ -0,0 +1,20 @@
+<script>
+  import SmearImage from "./SmearImage.svelte";
+
+  export let smearName;
+</script>
+
+<div class="row">
+  <button>Back</button>
+  <SmearImage {smearName} imageName={"pf220517213207.jpg"} />
+  <button>Forward</button>
+</div>
+
+<style>
+  .row {
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+    height: auto;
+  }
+</style>
