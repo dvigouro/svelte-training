30c73a648ac51635a4a8f430772185d83db20268
Denis Vigouroux <dvigouro@gmail.com>
2022-10-23 18:47:28 +0200

    Create SmearImage component

---
+++ src/lib/SmearImage.svelte
@@ -0,0 +1,17 @@
+<script>
+  export let imageName;
+  export let smearName;
+
+  $: imagePath = "/public/smears/" + smearName + "/images/" + imageName;
+</script>
+
+<div>
+  <img class="smearImage" src={imagePath} alt="" srcset="" />
+</div>
+
+<style>
+  .smearImage {
+    width: 100%;
+    height: auto;
+  }
+</style>
